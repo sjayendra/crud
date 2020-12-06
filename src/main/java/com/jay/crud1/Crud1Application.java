f6fc610a9bbabfdbb1f5863d98f9c7d9a82338c2
Jayendra.Sonsurkar <sjayendra@gmail.com>
2020-12-06 08:50:11 +0530

    first commit

--- src/main/java/com/jay/crud1/Crud1Application.java
+++ src/main/java/com/jay/crud1/Crud1Application.java
@@ -2,6 +2,14 @@ package com.jay.crud1;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.jay.crud1.User;
+import com.jay.crud1.UserRepository;
 
 @SpringBootApplication
 public class Crud1Application {
@@ -10,4 +18,21 @@ public class Crud1Application {
 		SpringApplication.run(Crud1Application.class, args);
 	}
 
+
+	@RequestMapping("/")
+	public String index() {
+		return "Greetings from Heroku !";
+	}
+
+	@RestController
+	@RequestMapping("/user")
+	public class UserController {
+		@Autowired
+		private UserRepository userRepository;
+
+		@GetMapping
+		public Iterable<User> getUser() {
+			return this.userRepository.findAll();
+		}
+	}
 }
